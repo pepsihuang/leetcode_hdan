e4726e3b0564cf446c6672a1383ac83491f891cd
alamu <skyhuangdan@qq.com>
2020-03-23 19:20:32 +0800

    增加注释

--- 236.二叉树的最近公共祖先.cpp
+++ 236.二叉树的最近公共祖先.cpp
@@ -16,6 +16,13 @@
  */
 class Solution {
 public:
+    /*
+    将树分为左右两边.
+    递归的查找,
+        当左边找到其中一个并且右边找到另外一个则此时的root就是答案
+        当左边找不到时,那两个肯定都在右边. (右边同理)
+        
+    */
 
     TreeNode* FindQorP(TreeNode* root,  TreeNode* p, TreeNode* q){
         if (!root || root->val == p->val || root->val == q->val)
@@ -26,18 +33,19 @@ public:
         TreeNode* left = FindQorP(root->left, p, q);
         TreeNode* right = FindQorP(root->right, p, q);
 
-        if (left == NULL)
+        if (left == NULL)//当root是叶子结点时,也就意味着left和right都是NULL
         {
             //都在右边
             return right;
         }
         else if(right == NULL)
         {
+            //都在左边
             return left;
         }
         else
         {
-            return root;
+            return root;//此处会直接找到
         }
         
         
